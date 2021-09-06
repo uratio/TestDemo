f5163bf64c92fedb0d959e64018ecfa4af32cb2d
yongliang.lang <yongliang.lang@bjrxtd.com>
2021-09-06 13:34:00 +0800

    添加groovy配置，新建java module 并命名 buildSRC 无效，必须引入buildSrc module，可在build.gradle中引用buildSrc中的配置

---
+++ buildSrc/src/main/groovy/Config.groovy
@@ -0,0 +1,20 @@
+class Config {
+
+    static applicationId = 'com.uratio.testdemo'
+    static appName = 'Test'
+    static compileSdkVersion = 31
+    static buildToolsVersion = '31.0.0'
+    static minSdkVersion = 19
+    static targetSdkVersion = 31
+    static versionCode = 1
+    static versionName = '1.0.0'
+
+    //测试
+    static API_TEST = "\"http://www.test.com/\""
+
+    //验证
+    static API_VERIFY = "\"http://www.yanzheng.com/\""
+
+    //生产
+    static API_RELEASE = "\"https://www.shengchan.com/\""
+}
