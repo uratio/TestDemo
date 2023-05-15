ec005969993345af4edf044a04b6f7fd08a57b32
uartio <18232586912@163.com>
2023-05-15 17:30:36 +0800

    交替位二进制数（简单）

--- app/src/test/java/com/uratio/testdemo/ArithmeticTest.java
+++ app/src/test/java/com/uratio/testdemo/ArithmeticTest.java
@@ -806,6 +806,21 @@ public class ArithmeticTest {
 
         return sb.toString();
     }
+
+    /**
+     * 693. 交替位二进制数
+     * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+     */
+    @Test
+    public void hasAlternatingBits() {
+        int number = 123456;
+        System.out.println("交替位二进制数：" + hasAlternatingBits(number));
+    }
+
+    private boolean hasAlternatingBits(int n) {
+        int a = n ^ (n >> 1);
+        return (a & (a + 1)) == 0;
+    }
 }
 
 class Automaton {
